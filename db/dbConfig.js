035aeaa38f5c6225c907d9f94bd7a792976fae23
Kimeme <133958988+Kimeme@users.noreply.github.com>
2025-01-07 20:12:58 +0300

    updated env and dbConfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,8 +4,27 @@ require("dotenv").config();
 // Disable SSL for local development, enable for production
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false, // Adjust based on your security requirements
+  },
 });
+
+async function connectToDatabase() {
+  try {
+    // Connect to the database
+    const client = await pool.connect();
+
+    // Test the connection by fetching the server version
+    const res = await client.query('SELECT version();');
+    console.log(`Connected to database. Server version: ${res.rows[0].version}`);
+
+    // Release the client back to the pool
+    client.release();
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
+  }
+}
+connectToDatabase();
 
 module.exports = pool;
 
