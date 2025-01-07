15d245edb5a7c49f702dd0811564204555229a71
Kimeme <133958988+Kimeme@users.noreply.github.com>
2025-01-07 18:24:28 +0300

    updated

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // Disable SSL for local development, enable for production
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  // ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
